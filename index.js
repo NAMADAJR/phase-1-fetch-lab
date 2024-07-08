569964236383eadf56b87a1b6e692de40615369e
Namada Junior <namada871@gmail.com>
2024-07-08 19:18:34 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+
+    // Handle fetch errors
+    .catch(error => console.error('Error fetching books:', error)); 
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  main.innerHTML = '';
+
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name; 
     main.appendChild(h2);
   });
 }
